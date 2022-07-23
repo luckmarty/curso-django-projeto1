f7c041381c4151d0ad5b540975ba478af8fbfbf1
Luciano Martínez Y Martínez <luckmarty@gmail.com>
2022-07-23 20:21:06 -0300

    Novos arquivos e atualizações

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Luciano'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Luciano'})
