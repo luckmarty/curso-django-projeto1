e9b74457b9e103e44e3796cd80b28f21b1441990
Luciano Martínez Y Martínez <luckmarty@gmail.com>
2022-08-03 22:49:03 -0300

    Ajustes dos templates e banco de dados

--- recipes/views.py
+++ recipes/views.py
@@ -4,8 +4,15 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+        })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         })
 
