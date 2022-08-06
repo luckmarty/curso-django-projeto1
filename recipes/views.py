19ae71cc02f7c7f024b18139885fe1616538ddef
Luciano Martínez Y Martínez <luckmarty@gmail.com>
2022-08-06 20:56:25 -0300

    Exibição de textos simples e html

--- recipes/views.py
+++ recipes/views.py
@@ -12,7 +12,9 @@ def home(request):
 
 def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id'))
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id'))
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
@@ -21,7 +23,12 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True
+    ).order_by('-id').first()
+    
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
         })
